a59fb7d5d2b1b78ceb9d08d26d31fb0ac34ea3d1
Ekorrigan <41920003+Ekorrigan@users.noreply.github.com>
2018-09-12 15:36:45 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,113 @@
+/* Variables globales */
+var numver = "0.1.1";
+var curver = "";
+/* IndexDB */
+var itemDB = {};
+var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+
+// Handle the prefix to IDBTransaction/IDBKeyRange.
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+
+itemDB.indexedDB = {};
+itemDB.indexedDB.db = null;
+
+itemDB.indexedDB.onerror = function(e) {
+	console.log(e);
+};
+
+itemDB.indexedDB.open = new Promise( function(resolve,reject){
+	console.log("opdenDB : Début");
+	var request = indexedDB.open("testPOC");
+	request.onerror = function(event) {
+	  console.error("openDb:", event.target.errorCode);
+	};
+	request.onsuccess = function(event) {
+		itemDB.indexedDB.db = this.result;
+		console.log("openDb DONE");
+		resolve(itemDB.indexedDB.db)
+	};
+	request.onupgradeneeded = function(event) { 
+	  console.log("openDb Upgrade");
+	  itemDB.indexedDB.db = this.result;
+	  // Crée un objet de stockage pour cette base de données
+	  var objectStore = itemDB.indexedDB.db.createObjectStore("version", { keyPath: "numver" });
+		objectStore.transaction.oncomplete = function(evt) {
+		// Stocker les valeurs dans le nouvel objet de stockage.
+		var verObjectStore = itemDB.indexedDB.db.transaction("version", "readwrite").objectStore("version");
+		verObjectStore.add({numver : numver});
+	  }
+	};
+	console.log("opdenDB : Fin");
+});
+
+//Ouverture de la base
+function init() {
+	itemDB.indexedDB.open.then(loaded).catch(function(){console.log("erreur de chargement");});
+}
+window.addEventListener("DOMContentLoaded", init, false);
+/* Fonctions */
+function updateOnlineStatus() {
+   var etat = navigator.onLine ? "online" : "offline";
+   var status = document.getElementById("status");
+   var lnkup = document.getElementById("upgrade");
+   status.src="/images/icon/"+etat+".png";
+   status.alt=etat;
+   if(lnkup){
+	   if(etat == "offline"){
+			lnkup.style.display = "none";
+	   }
+	   else{
+			lnkup.style.display = "";
+	   }
+   }
+   console.log(etat);
+}
+ 
+function loaded(db) {
+	console.log("Loaded : Début");
+	var version;
+	var objectStore = db.transaction("version").objectStore("version");
+	objectStore.openCursor().onsuccess = function(event) {
+		var cursor = event.target.result;
+		if (cursor) {
+			curver = cursor.value.numver;
+			console.log("récupération version : " + curver);
+		}
+		else{
+			console.log("pas de version courante");
+			curver = numver;
+			var verObjectStore = db.transaction("version", "readwrite").objectStore("version");
+			verObjectStore.add({numver : numver});
+		}
+		document.getElementsByTagName("footer")[0].innerHTML="Version : " + curver;
+		if (curver!=numver) {
+			if ("serviceWorker" in navigator) {
+				navigator.serviceWorker.controller.postMessage(false);
+			}
+			console.log("nouvelle version disponible :  "+curver+" -> "+numver);
+			document.getElementsByTagName("footer")[0].insertAdjacentHTML("beforeend", "<a href='#' id='upgrade' onclick='upgrade(); return false;'> Cliquez ici pour passer à la version " + numver + "</a>");
+			updateOnlineStatus();
+		}
+		if ("serviceWorker" in navigator) {
+			navigator.serviceWorker.controller.postMessage(curver==numver);
+		}
+	};
+   updateOnlineStatus();
+   window.addEventListener("offline",updateOnlineStatus);
+   window.addEventListener("online",updateOnlineStatus);
+   console.log("Loaded : Fin");
+}
+
+function upgrade(){
+	curver = numver;			
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker.controller.postMessage(true);
+	}
+	var objectStore = itemDB.indexedDB.db.transaction("version", "readwrite").objectStore("version");
+	var objectStoreRequest = objectStore.clear(); 
+	objectStoreRequest.onsuccess = function(event) { 
+		window.location.reload();
+	}; 
+}
+	 
---
+++ sw.js
@@ -0,0 +1,104 @@
+var uptodate = true;
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+if (workbox) {
+	console.log(`Yay! Workbox is loaded 🎉         -     uptodate :` + uptodate);
+  workbox.precaching.precacheAndRoute([
+  {
+    "url": "index.html",
+    "revision": "e786abbb42629a288ae2b1c5b8e747ad"
+  },
+  {
+    "url": "style/main.css",
+    "revision": "c21b1d64efe5fa91addbd872b7e5137b"
+  },
+  {
+    "url": "js/main.js",
+    "revision": "d699b8ab1e4d91cf6fc201977d00a5f9"
+  },
+  {
+    "url": "js/animation.js",
+    "revision": "3f8fd475afa44c10b3107178a83bd9ae"
+  },
+  {
+    "url": "images/icon/icon.svg",
+    "revision": "d582b402cdafcc4a3934fba3986d1be7"
+  },
+  {
+    "url": "images/icon/offline.png",
+    "revision": "98719d2bea65dce664e52ae10074e616"
+  },
+  {
+    "url": "images/icon/online.png",
+    "revision": "7ada381259d03c6cca6b8e9d672e94f9"
+  },
+  {
+    "url": "pages/offline.html",
+    "revision": "60fdf44d5438078f9f242d504f013941"
+  },
+  {
+    "url": "pages/404.html",
+    "revision": "85695da32717e30feceb88d152194353"
+  }
+]);
+  const NetWorkHandler = workbox.strategies.staleWhileRevalidate({
+    cacheName: 'diag-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      })
+    ]
+  });
+    const OfflineHandler = workbox.strategies.cacheOnly({
+    cacheName: 'poc-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      })
+    ]
+  });
+  workbox.routing.registerRoute(
+    /.*\.(?:png|gif|jpg|svg)/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        })
+      ]
+    })
+  );
+  workbox.routing.registerRoute(/.*\.(?:html|htm|js)/, args => {
+	  console.log(args);
+	console.log("up to date : " + uptodate);
+	if(navigator.onLine){
+		return NetWorkHandler.handle(args).then(response => {
+		  if (!response) {
+			return caches.match('pages/offline.html');
+		  } else if (response.status === 404) {
+			return caches.match('pages/404.html');
+		  }
+		  return response;
+		});
+	} else {
+		return OfflineHandler.handle(args).then(response => {
+		  if (response.status === 404) {
+			return caches.match('pages/404.html');
+		  }
+		  return response;
+		})
+		.catch(function() {
+		  return caches.match('pages/offline.html');
+		});
+	}
+  });
+  
+  self.addEventListener('message', function(event){
+	  uptodate = event.data;
+	console.log("SW Received Message: " + event.data);
+  });
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
